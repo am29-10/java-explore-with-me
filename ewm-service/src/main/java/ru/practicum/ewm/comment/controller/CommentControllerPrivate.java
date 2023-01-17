4dd888212b07a0c9095215746a6dd86424d3cacc
Artur <am29-10@mail.ru>
2023-01-17 18:46:55 +0300

    Убрать общий импорт

--- ewm-service/src/main/java/ru/practicum/ewm/comment/controller/CommentControllerPrivate.java
+++ ewm-service/src/main/java/ru/practicum/ewm/comment/controller/CommentControllerPrivate.java
@@ -2,8 +2,15 @@ package ru.practicum.ewm.comment.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import ru.practicum.ewm.comment.dto.CommentDto;
 import ru.practicum.ewm.comment.dto.NewCommentDto;
 import ru.practicum.ewm.comment.mapper.CommentMapper;
@@ -17,7 +24,6 @@ import java.util.List;
 @Slf4j
 @AllArgsConstructor
 @RequestMapping("/users/{userId}/events/{eventId}/comments")
-@Validated
 public class CommentControllerPrivate {
 
     private final CommentService commentService;
--- ewm-service/src/main/java/ru/practicum/ewm/comment/controller/CommentControllerPublic.java
+++ ewm-service/src/main/java/ru/practicum/ewm/comment/controller/CommentControllerPublic.java
@@ -2,7 +2,11 @@ package ru.practicum.ewm.comment.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.practicum.ewm.comment.dto.CommentDto;
 import ru.practicum.ewm.comment.service.CommentService;
 
